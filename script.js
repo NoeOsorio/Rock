db8c69b184caa8ad2ce0a11c984dba9fcc0156f9
Noe Osorio <osorionoe@gmail.com>
2018-06-04 14:16:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,23 +8,25 @@ function computerplay() {
         return "paper";
         break;
     case 3 :
-        return "scizors";
+        return "scissors";
         break;
     default:
-        return "rock";
+        return "paper";
 
   }
 
 }
 function playerplay() {
-  var play = prompt();
-
+  var play = prompt("Rock Scissors Paper");
+  play = play.toLowerCase();
   return play;
 }
 
 function game(player, computer){
+  console.log("Computer: " + computer);
+  console.log("You: " + player);
   switch (player) {
-    case /ROCK/i:
+    case "rock":
         if (computer == "rock") {
           return "Draw"
         }
@@ -35,9 +37,57 @@ function game(player, computer){
           return "You Won"
         }
       break;
+      case "paper":
+          if (computer == "paper") {
+            return "Draw"
+          }
+          else if (computer == "scissors") {
+              return "You Lose!"
+          }
+          else {
+            return "You Won"
+          }
+        break;
+        case "scissors":
+            if (computer == "scissors") {
+              return "Draw"
+            }
+            else if (computer == "rock") {
+                return "You Lose!"
+            }
+            else {
+              return "You Won"
+            }
+          break;
     default:
       return "Just play!!"
 
   }
 }
-console.log(game(playerplay(),computerplay()));
+function vs() {
+  var player = 0, computer = 0;
+  var winner;
+  for (var i = 0; i < 5; i++) {
+    var gameplayer = playerplay(), gamecomputer = computerplay();
+    var result = game(gameplayer,gamecomputer);
+    if (result.search(/won/i) >= 0) {
+      player++;
+    }
+    else if (result.search(/lose/i) >= 0){
+      computer++;
+    }
+    console.log(result);
+  }
+  if (player > computer){
+    winner = "player";
+  }
+  else if (player < computer){
+    winner = "computer";
+  }
+  else {
+    winner = "Draw";
+  }
+  window.alert("Player: " + player + "\n" + "Computer: " + computer + "\n" +  "Winner: " + winner);
+}
+//console.log(game(playerplay(),computerplay()));
+vs();
